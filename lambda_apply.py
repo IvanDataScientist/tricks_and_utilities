81a39b40f9094a55e9c93dd760a9b1a12a7af1ad
unknown <ivan@CienciayDatos.es>
2020-09-02 10:36:30 +0200

    version 2, pandas vs apply, not abuse apply

--- lambda_apply.py
+++ lambda_apply.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import time
 df = pd.DataFrame(np.random.randint(0, 100, size=(4, 2)), columns=list('AB'))
 print(df)
 print(df.apply(lambda x: x + 1))
@@ -15,3 +16,27 @@ print(df.apply(lambda x: x + 1))
 #1  75  68
 #2  96   7
 #3  99  12
+
+df.apply(np.sum, axis=1) # sum columns
+# OUTPUT
+#0     62
+#1    141
+#2    101
+#3    109
+#dtype: int64
+
+start=time.time() # view times
+df.apply(np.mean) # mean total columns
+print(time.time()-start)
+# OUTPUT
+#A    79.50
+#B    23.75
+#dtype: float64
+
+start=time.time()
+df.mean() # not apply
+print(time.time()-start)
+
+# apply vs pandas times
+# 0.00498652458190918     apply
+# 0.0009996891021728516   pandas
