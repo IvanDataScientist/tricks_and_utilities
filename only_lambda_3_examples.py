f6fa1d00342c167dd1590e4eecc008ceb5b16a4c
unknown <ivan@CienciayDatos.es>
2020-09-03 07:49:04 +0200

    v1 3 examples

---
+++ only_lambda_3_examples.py
@@ -0,0 +1,19 @@
+
+#Example:1
+#Easy function anonymous
+impar = lambda numero: numero%2 != 0
+impar(5)
+# OUT: True
+
+#Example:2
+revertir = lambda cadena: cadena[::-1]
+revertir("Plone")
+#OUT:'enolP'
+revertir("enolP")
+#OUT:'Plone'
+
+#Example:3
+#Two Parameters
+sumar = lambda x,y: x+y
+sumar(5,2)
+#OUT: 7
