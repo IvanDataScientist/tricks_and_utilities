b36a46fe627b16316dfdffbdcaa2ce8bd89dc029
unknown <ivan@CienciayDatos.es>
2020-09-03 09:51:13 +0200

    v2 clean code

--- lambda_filter_vs_def_compare_times.py
+++ lambda_filter_vs_def_compare_times.py
@@ -5,14 +5,15 @@ import timeit
 
 #List 1000 first numbers
 comprobar = range(1000)
+
 start=(time.time())
 #Var + filter() + lambda
 filt = filter(lambda x: x % 2 == 0, comprobar)
 #Result to list 
 pares = list(filt)
 #Show list if filter is True
-print(pares,time.time()-start)
-#OUTPUT [2, 4, 6, 8, 10 ....]
+print(pares[1:5],time.time()-start)
+#OUTPUT [2, 4, 6, 8....]
 
 
 start=(time.time())
@@ -25,6 +26,5 @@ def function(comprobar):
         if x % 2 == 0:
             par.append(x)
     return(par)
-
-print(function(comprobar),(time.time()-start))
-#OUTPUT [2, 4, 6, 8, 10 ....]
+print(function(comprobar)[1:5],(time.time()-start))
+#OUTPUT [2, 4, 6, 8....]
