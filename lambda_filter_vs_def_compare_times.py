ad68b648112acf61d1da784330790933311f650a
unknown <ivan@CienciayDatos.es>
2020-09-03 09:29:10 +0200

    v1. lambda + filter vs only def + for

---
+++ lambda_filter_vs_def_compare_times.py
@@ -0,0 +1,30 @@
+
+import timeit
+
+#Example filter + lambda
+
+#List 1000 first numbers
+comprobar = range(1000)
+start=(time.time())
+#Var + filter() + lambda
+filt = filter(lambda x: x % 2 == 0, comprobar)
+#Result to list 
+pares = list(filt)
+#Show list if filter is True
+print(pares,time.time()-start)
+#OUTPUT [2, 4, 6, 8, 10 ....]
+
+
+start=(time.time())
+#Example only def
+def function(comprobar):
+    """
+    """
+    par=[]
+    for x in comprobar:
+        if x % 2 == 0:
+            par.append(x)
+    return(par)
+
+print(function(comprobar),(time.time()-start))
+#OUTPUT [2, 4, 6, 8, 10 ....]
